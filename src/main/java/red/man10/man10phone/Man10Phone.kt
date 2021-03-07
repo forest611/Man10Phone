a0dd570c4388030693e93cb485aaa313e350f8f6
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2021-03-07 23:08:22 +0900

    chore: update to 1.16.3

--- src/main/java/red/man10/man10phone/Man10Phone.kt
+++ src/main/java/red/man10/man10phone/Man10Phone.kt
@@ -1,5 +1,9 @@
 package red.man10.man10phone
 
+import com.github.syari.spigot.api.util.item.displayName
+import net.md_5.bungee.api.chat.BaseComponent
+import net.md_5.bungee.api.chat.ComponentBuilder
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.command.Command
@@ -11,13 +15,14 @@ import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import java.awt.Component
 
-class Man10Phone : JavaPlugin() ,Listener {
+class Man10Phone : JavaPlugin(), Listener {
 
 
     val apps = HashMap<Int,App>()
     lateinit var menu : Inventory
-    val version = "mOS 1.2"
+    val version = "mOS 14.0"
 
     override fun onEnable() {
         saveDefaultConfig()
@@ -29,7 +34,7 @@ class Man10Phone : JavaPlugin() ,Listener {
 
     }
 
-    override fun onCommand(sender: CommandSender?, command: Command?, label: String?, args: Array<out String>?): Boolean {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 
         if (sender !is Player){
             return false
@@ -37,16 +42,16 @@ class Man10Phone : JavaPlugin() ,Listener {
 
         if (!sender.hasPermission("mphone.op"))return true
 
-        if (args == null || args.isEmpty()){
+        if (args.isEmpty()){
             openPhone(sender)
             return true
         }
 
         if (args[0] == "reload"){
-            Thread(Runnable {
+            Thread {
                 loadConfig()
                 sender.sendMessage("§a§lリロード完了！")
-            }).start()
+            }.start()
             return true
         }
 
@@ -65,9 +70,9 @@ class Man10Phone : JavaPlugin() ,Listener {
 
         for (app in apps){
             val a = app.value
-            val icon = ItemStack(a.type,1,a.damage.toShort())
+            val icon = ItemStack(a.type,1)
             val meta = icon.itemMeta
-            meta.displayName = a.title
+            icon.displayName = a.title
             meta.lore = a.lore
             icon.itemMeta =meta
             inv.setItem(slot[app.key],icon)
@@ -83,21 +88,21 @@ class Man10Phone : JavaPlugin() ,Listener {
         for (i in 0..11){
             val app = App()
 
-            app.title = config.getString("$i.title")
+            app.title = config.getString("$i.title")!!
             app.lore = config.getStringList("$i.lore")
-            app.type = Material.valueOf(config.getString("$i.type"))
+            app.type = Material.valueOf(config.getString("$i.type")!!)
             app.damage = config.getInt("$i.damage",0)
-            app.cmd = config.getString("$i.cmd")
+            app.cmd = config.getString("$i.cmd")!!
 
             apps[i] = app
         }
     }
 
     fun createHomeMenu():Inventory{
-        val inv = Bukkit.createInventory(null,54,version)
+        val inv = Bukkit.createInventory(null, 54, "")
 
-        val pane1 = ItemStack(Material.STAINED_GLASS_PANE,1,7)
-        val pane2 = ItemStack(Material.STAINED_GLASS_PANE,1,3)
+        val pane1 = ItemStack(Material.WHITE_STAINED_GLASS_PANE,1)
+        val pane2 = ItemStack(Material.WHITE_STAINED_GLASS_PANE,1)
 
 
         val list1 = mutableListOf(0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,44,45,53)
@@ -115,13 +120,13 @@ class Man10Phone : JavaPlugin() ,Listener {
 
     @EventHandler
     fun inventoryClick(e:InventoryClickEvent){
-        if (e.inventory.title != version)return
+        if (e.whoClicked.openInventory.title != version)return
         val player = e.whoClicked as Player
         e.isCancelled = true
         if (e.currentItem == null)return
-        if (e.currentItem.itemMeta == null)return
+        if (e.currentItem!!.itemMeta == null)return
         for (a in apps.values){
-            if (a.title == e.currentItem.itemMeta.displayName){
+            if (a.title == e.currentItem!!.itemMeta.displayName){
                 if (!player.isOp){
                     player.isOp = true
                     player.performCommand(a.cmd)
